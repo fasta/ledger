4f1b8e1052bf6e4b6bfb1baf290743661a78cb8c
Fabian Staubli <fabian@previous.li>
2015-06-10 23:31:42 +0200

    Add support for account hierarchy to balancing

--- lib/ledger.rb
+++ lib/ledger.rb
@@ -19,12 +19,33 @@ module Ledger
     raise ArgumentError unless transactions.select {|t| !t.complete? }.empty?
     raise ArgumentError unless transactions.select {|t| !t.balanced? }.empty?
 
-    transactions.reduce({}) do |accounts, tx|
+    accounts = transactions.reduce({}) do |accounts, tx|
       tx.postings.each do |p|
         accounts[p.account] = (accounts[p.account]) ? accounts[p.account] + p.amount : p.amount
       end
 
       accounts
+    end
+
+    accounts.keys.map {|k| account_hierarchy(k) }.flatten.uniq.each do |superior|
+      total = accounts.select {|name, amount| name.start_with?(superior) }
+        .map {|name, amount| amount }.reduce(:+)
+
+      accounts[superior] = total
+    end
+
+    accounts
+  end
+
+  def self.account_hierarchy(name, separator=':')
+    name.split(separator).reduce([]) do |memo, part|
+      if memo.last
+        memo << [memo.last, part].join(':')
+      else
+        memo << part
+      end
+
+      memo
     end
   end
 
--- spec/ledger_spec.rb
+++ spec/ledger_spec.rb
@@ -41,6 +41,43 @@ describe Ledger do
         'Equity' => Amount.from_s('$-100.00')
       })
     end
+
+    it "should return a Hash including super-accounts" do
+      txs = [
+        Transaction.new(postings: [Posting.from_s('Assets:Bank:Bank A   $100.00'),
+                                   Posting.from_s('Assets:Bank:Bank B   $200.00'),
+                                   Posting.from_s('Equity:Opening Balances')]).complete!,
+        Transaction.new(postings: [Posting.from_s('Expenses   $75.00'),
+                                   Posting.from_s('Assets:Bank:Bank A')]).complete!
+      ]
+
+      Ledger.balance(txs).must_equal({
+        'Assets' => Amount.from_s('$225.00'),
+        'Assets:Bank' => Amount.from_s('$225.00'),
+        'Assets:Bank:Bank A' => Amount.from_s('$25.00'),
+        'Assets:Bank:Bank B' => Amount.from_s('$200.00'),
+        'Equity' => Amount.from_s('$-300.00'),
+        'Equity:Opening Balances' => Amount.from_s('$-300.00'),
+        'Expenses' => Amount.from_s('$75.00')
+      })
+    end
+  end
+
+  describe ".account_hierarchy" do
+    it "should return an Array of each individual account contained in the name" do
+      Ledger.account_hierarchy('Assets').must_equal ['Assets']
+
+      Ledger.account_hierarchy('Assets:Checking').must_equal [
+        'Assets',
+        'Assets:Checking'
+      ]
+
+      Ledger.account_hierarchy('Assets:Checking:Business').must_equal [
+        'Assets',
+        'Assets:Checking',
+        'Assets:Checking:Business'
+      ]
+    end
   end
 
 end
