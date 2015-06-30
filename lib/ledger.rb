f119583b5e96e0afd0dd89f75905d99fb8526b74
Fabian Staubli <fabian@previous.li>
2015-06-30 14:56:58 +0200

    Remove unnecessary methods and tests

--- lib/ledger.rb
+++ lib/ledger.rb
@@ -14,42 +14,5 @@ require 'ledger/account'
 
 
 
-module Ledger
-
-  def self.balance(transactions)
-    raise ArgumentError unless transactions.select {|t| !t.complete? }.empty?
-    raise ArgumentError unless transactions.select {|t| !t.balanced? }.empty?
-
-    accounts = transactions.reduce({}) do |accounts, tx|
-      tx.postings.each do |p|
-        accounts[p.account_name] =
-          (accounts[p.account_name]) ? accounts[p.account_name] + p.amount : p.amount
-      end
-
-      accounts
-    end
-
-    accounts.keys.map {|k| account_hierarchy(k) }.flatten.uniq.each do |superior|
-      total = accounts.select {|name, amount| name.start_with?(superior) }
-        .map {|name, amount| amount }.reduce(:+)
-
-      accounts[superior] = total
-    end
-
-    accounts
-  end
-
-  def self.account_hierarchy(name, separator=':')
-    name.split(separator).reduce([]) do |memo, part|
-      if memo.last
-        memo << [memo.last, part].join(':')
-      else
-        memo << part
-      end
-
-      memo
-    end
-  end
-
-end
+module Ledger; end
 
